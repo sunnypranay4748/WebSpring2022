701e75cfe41ebe72a46ea4e6c976f6adffb0ca4a
sunnypranay4748 <98367465+sunnypranay4748@users.noreply.github.com>
2022-02-18 19:37:14 -0600

    added docs from the assignment link

---
+++ WebPart/ICP5/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  
+  // define list of items
+  items = ['Task 1', 'Task 2', 'Task 3'];
+  counter = 0;
+  newItem = '';
+  title = 'ToDoList';
+
+  // Write code to push new item
+  submitNewItem() {
+    if (this.newItem !== '') {
+
+      this.items.push(this.newItem); 
+      this.newItem = '';
+    }
+
+    document.getElementById('addItem').focus();
+  }
+
+  // Write code to complete item
+  completeItem(item: any, i: any) {
+
+    document.getElementById(i).innerHTML = '<del>'  + item + '</del>';
+
+  }
+
+  // Write code to delete item
+  deleteItem(i: any) {
+
+    this.items.splice(i,1);
+
+  }
+
+}
---
+++ WebPart/ICP5/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
