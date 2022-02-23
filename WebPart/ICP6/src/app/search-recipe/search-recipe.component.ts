90e1edc254bcefcea3733d4d5c86882ce07daeb3
sunnypranay4748 <98367465+sunnypranay4748@users.noreply.github.com>
2022-02-22 19:50:40 -0600

    changed background and added api logic for search receipe component

--- WebPart/ICP6/src/app/search-recipe/search-recipe.component.ts
+++ WebPart/ICP6/src/app/search-recipe/search-recipe.component.ts
@@ -1,6 +1,70 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+/* created interface for venue search */
+
+interface VenueSearchResponse {
+  meta: {};
+  response: {
+    venues: Venue[];
+    confident: boolean;
+  };
+}
+
+/* created interface for recipe search */
+
+interface RecipeSearchResponse {
+  count: number;
+  from: number;
+  hits: Hit[];
+  more: boolean;
+  q: string;
+  to: number;
+}
+/* created interface to track hits*/
+
+interface Hit {
+  recipe: Recipe;
+}
+/* Venue interface */
+interface Venue {
+  id: string;
+  name: string;
+  location: {
+    address: string;
+    postalCode: string;
+    city: string;
+    state: string;
+    country: string;
+    formattedAddress: string[];
+  };
+  categories: {
+    name: string;
+  };
+  referralId: string;
+  hasPerk: boolean;
+}
+/* recipe interface */
+
+interface Recipe {
+  calories: number;
+  dietLabels: string[];
+  healthLabels: string[];
+  image: string | null;
+  ingredientLines: string[];
+  ingredients: {
+    image: string | null;
+    text: string;
+    weight: number;
+  }[];
+  label: string;
+  source: string;
+  totalTime: number;
+  uri: string;
+  url: string;
+  yield: number;
+}
+
 @Component({
   selector: 'app-search-recipe',
   templateUrl: './search-recipe.component.html',
@@ -12,7 +76,7 @@ export class SearchRecipeComponent implements OnInit {
   recipeValue: any;
   placeValue: any;
   venueList = [];
-  recipeList = [];
+  recipeList: Recipe[];
 
   currentLat: any;
   currentLong: any;
@@ -32,20 +96,25 @@ export class SearchRecipeComponent implements OnInit {
   }
 
   getVenues() {
-
     this.recipeValue = this.recipes.nativeElement.value;
     this.placeValue = this.places.nativeElement.value;
-
     if (this.recipeValue !== null) {
-      /**
-       * Write code to get recipe
-       */
+      
+      this._http.get('https://api.edamam.com/search?q=' + this.recipeValue + '&app_id=6b2ccc59'
+        + '&app_key=835422279b4e65d2d1de1fa5245061a8')
+          .subscribe(({hits}: RecipeSearchResponse) => {
+            this.recipeList = hits.map(hit => hit.recipe);
+          });
     }
-
-    if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
-      /**
-       * Write code to get place
-       */
+    if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null) {
+      if (this.recipeValue !== '') {
+        
+        this._http.get('https://api.foursquare.com/v2/venues/search?client_id=HVNZDHZ3Y2FFR0ZSADDZBP3N30YEA1DZQYOGG35VCUCBUOT4' + '&client_secret=1QTPX3WSGSUFTO0VAGMNEYEJFPDFQO15RGJ3C5RINYJZATWH&v=20180323&limit=10&near=' + this.placeValue + '&query=' + this.recipeValue).
+        subscribe(({response}: VenueSearchResponse) => {
+          this.venueList = response.venues;
+        });
+      }
     }
   }
+
 }
